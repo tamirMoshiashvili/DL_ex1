# fix the first part, there was a problem with the numeric gradient
# complete part 2 of assignment

code/grad_check.py
@@ -5,8 +5,8 @@ STUDENT = {'name': 'Tamir Moshiashvili',
 
 
 def gradient_check(f, x):
-    """ 
-    Gradient check for a function f 
+    """
+    Gradient check for a function f
     - f should be a function that takes a single argument and outputs the cost and its gradients
     - x is the point (numpy array) to check the gradient at
     """
@@ -18,7 +18,11 @@ def gradient_check(f, x):
     while not it.finished:
         ix = it.multi_index
 
-        numeric_gradient = f(x[ix] + h)[0] - f(x[ix] - h)[0]
+        x_plus = np.copy(x)
+        x_plus[ix] += h
+        x_minus = np.copy(x)
+        x_minus[ix] -= h
+        numeric_gradient = f(x_plus)[0] - f(x_minus)[0]
         numeric_gradient /= 2 * h
 
         # Compare gradients

code/loglinear.py
@@ -24,7 +24,7 @@ def classifier_output(x, params):
     """
     W, b = params
 
-    probs = softmax(np.dot(W, x) + b)
+    probs = softmax(np.dot(x, W) + b)
 
     return probs
 
@@ -52,14 +52,18 @@ def loss_and_gradients(x, y, params):
     W, b = params
 
     # YOU CODE HERE
-    loss = -(classifier_output(x, params)[y])
     probs = classifier_output(x, params)
+    loss = -np.log(probs[y])
 
-    gW = x * probs
-    gW[y] -= x[y]
+    # gradient of b
+    gb = np.copy(probs)
+    gb[y] -= 1
 
-    probs[y] -= 1
-    gb = probs
+    # gradient of W
+    gW = np.zeros(W.shape)
+    # grad of W[i, j] = xi * y_hat[j] - y[j]
+    for (i, j) in np.ndindex(W.shape):
+        gW[i, j] = x[i] * probs[j] - (j == y) * x[i]
 
     return loss, [gW, gb]
 